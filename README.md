d4d66b9315be1f1bf5d2bcb68f8b551a88293c6e
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-01-23 11:22:02 -0400

    Feat. Create a reactive label connected/disconnected
    We created a reactive label that tells if the server is connected or disconnected.
    - Remember that socket on is for listen
    - Remember that socket emit is for send something to the server (in the client side).

---
+++ README.md
@@ -0,0 +1,15 @@
+# Socket Client  (Teslo Shop)
+
+This app connects with our socket instance of the Teslo Shop backend.
+
+## Installation
+- Install dependencies
+```
+npm install
+```
+
+- run app in development
+```
+npm run dev
+```
+
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,6 +1,22 @@
-import { Manager } from "socket.io-client";
+import { Manager, Socket } from "socket.io-client";
 export const connectToServer = () => {
   const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
 
   const socket = manager.socket("/");
+
+  addListener(socket);
+};
+
+const addListener = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector("#server-status")!; // the '!' sign means that we are assure that the component will exists
+
+  socket.on("connect", () => {
+    console.log("connected...");
+    serverStatusLabel.innerHTML = "connected";
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected...");
+    serverStatusLabel.innerHTML = "disconnected";
+  });
 };
