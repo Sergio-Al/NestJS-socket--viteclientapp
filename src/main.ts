1656a9760dc01d6a21380313cf1485df6a404402
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-01-23 10:45:05 -0400

    Feat. First commit SocketIO
    We configured our vite vanilla javascript app.
    We are using socket io client to make a connection to our teslo shop socket backend.

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { connectToServer } from './socket-client';
+import "./style.css";
+// import typescriptLogo from "./typescript.svg";
+// import { setupCounter } from "./counter";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Websocket -  Client</h1>
+    <span>offline</span>
+  </div>
+`;
+
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+connectToServer()
